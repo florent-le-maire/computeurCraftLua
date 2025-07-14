b61cd7f98314e014b0a9f7544cc85cec55788cc3
florent-le-maire <lemaireflorent9@gmail.com>
2025-07-14 22:11:15 +0200

    [WIP] test functionning

--- cuisson.lua
+++ cuisson.lua
@@ -1,14 +1,12 @@
--- Slot 1 : Charbon
--- Slots 2 à 16 : Patates et carottes
-
-while true do
-    turtle.turnRight()
-    -- 1. Tente de prendre un item à la fois pour filtrer immédiatement
-    for i = 2, 16 do
+local function take(fromX, toY)
+    for i = fromX, toY do
         turtle.select(i)
         turtle.suck()
     end
-    for i = 2, 16 do
+end
+
+local function filter(fromX, toY)
+    for i = fromX, toY do
         turtle.select(i)
         local item = turtle.getItemDetail()
         if item then
@@ -18,45 +16,56 @@ while true do
             end
         end
     end
+end
+local function takeAndFilter()
+    local fromX = 2
+    local toY = 16
+    take(fromX, toY)
+    filter(fromX, toY)
+end
+local function countThisIndex(i)
+    turtle.select(i)
+    local item = turtle.getItemDetail()
+    if item then
+        return item.count
+    end
+    return 0
+end
+
+local function countForThisRange(fromX, toY)
+    local count = 0
+    for i = fromX, toY do
+        count = count + countThisIndex(i)
+    end
+    return count
+end
+
+local function dropItemToSmelt(i, count)
+    turtle.select(i)
+    local item = turtle.getItemDetail()
+    if item then
+        return turtle.dropUp(count)
+    end
+    return false
+end
+local function dropCoal()
+    turtle.select(1)
+    return turtle.drop(1)
+end
+
+local function dropCoalAndSmelt(countItems)
+    
+end
+
+
+-- Boucle principale
+while true do
+    turtle.turnRight()
+    takeAndFilter()
     turtle.turnLeft()
     turtle.turnLeft()
-
-    -- 2. Compter le nombre de patates
-    local potatoCount = 0
-    for i = 2, 16 do
-        turtle.select(i)
-        local item = turtle.getItemDetail()
-        if item and item.name == "minecraft:potato" then
-            potatoCount = potatoCount + item.count
-        end
-    end
-
-    -- 3. Si on a au moins 8 patates, les déposer + 1 charbon
-    while potatoCount >= 8 do
-        local deposited = 0
-        local coal = 0
-        for i = 2, 16 do
-            if deposited >= 8 then break end
-            turtle.select(i)
-            local item = turtle.getItemDetail()
-            if item then
-                turtle.dropUp(8)
-                deposited = deposited + 8
-                potatoCount = potatoCount - 8
-            end
-        end
-
-        -- Mettre 1 charbon
-        turtle.select(1)
-        if turtle.getItemCount() > 0 and deposited % 8 > coal then
-            turtle.drop(1)
-            coal = coal + 1
-        else
-            print("⚠️ Plus de charbon !")
-        end
-
-        sleep(1)
-    end
-
+    local countItems = countForThisRange(2, 16)
+    dropCoalAndSmelt(countItems)
+    turtle.turnRight()
     sleep(5)
 end
