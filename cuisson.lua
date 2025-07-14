8a322243644b18e49d87f05883eb6dbbc4a6c1ad
florent-le-maire <lemaireflorent9@gmail.com>
2025-07-14 21:22:38 +0200

    Fix coal deposit

--- cuisson.lua
+++ cuisson.lua
@@ -2,7 +2,7 @@
 -- Slots 2 à 16 : Patates et carottes
 
 while true do
-    turtle.turnLeft()
+    turtle.turnRight()
     -- 1. Tente de prendre un item à la fois pour filtrer immédiatement
     for i = 2, 16 do
         turtle.select(i)
@@ -18,8 +18,8 @@ while true do
             end
         end
     end
-    turtle.turnRight()
-    turtle.turnRight()
+    turtle.turnLeft()
+    turtle.turnLeft()
 
     -- 2. Compter le nombre de patates
     local potatoCount = 0
@@ -34,22 +34,23 @@ while true do
     -- 3. Si on a au moins 8 patates, les déposer + 1 charbon
     while potatoCount >= 8 do
         local deposited = 0
+        local coal = 0
         for i = 2, 16 do
             if deposited >= 8 then break end
             turtle.select(i)
             local item = turtle.getItemDetail()
             if item then
-                local toDrop = math.min(item.count, 8 - deposited)
-                turtle.drop(toDrop)
-                deposited = deposited + toDrop
-                potatoCount = potatoCount - toDrop
+                turtle.drop(8)
+                deposited = deposited + 8
+                potatoCount = potatoCount - 8
             end
         end
 
         -- Mettre 1 charbon
         turtle.select(1)
-        if turtle.getItemCount() > 0 then
+        if turtle.getItemCount() > 0 and deposited % 8 < coal then
             turtle.drop(1)
+            coal = coal + 1
         else
             print("⚠️ Plus de charbon !")
         end
